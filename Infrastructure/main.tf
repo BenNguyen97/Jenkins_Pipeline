4759c24d69de15c001631b611b3a015b2d5c9d23
Luan Nguyen <114214306+BenNguyen97@users.noreply.github.com>
2025-02-16 16:59:21 +0700

    cập nhật public_subnet

--- Infrastructure/main.tf
+++ Infrastructure/main.tf
@@ -11,5 +11,5 @@ module "vpc" {
   cidr = "10.0.0.0/16"
 
   azs             = ["ap-southeast-1a", "ap-southeast-1b", "ap-southeast-1c"]
-  public_subnets  = ["10.0.0.0/24"]
+  public_subnets  = ["10.0.1.0/24", "10.0.2.0/24", "10.0.3.0/24"]
 }
